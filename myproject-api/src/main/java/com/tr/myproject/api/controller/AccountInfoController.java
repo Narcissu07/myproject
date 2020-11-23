428ade16cc37f9b1a1705a8b7944aa6ae07bc530
pengjun <pengjun@sincere.com.cn>
2020-11-23 16:17:38 +0800

    rocekMQ 事物消息测试

--- myproject-api/src/main/java/com/tr/myproject/api/controller/AccountInfoController.java
+++ myproject-api/src/main/java/com/tr/myproject/api/controller/AccountInfoController.java
@@ -5,6 +5,7 @@ import com.tr.myproject.service.request.AccountChangeEvent;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -19,6 +20,7 @@ import java.util.UUID;
  **/
 @RestController
 @Slf4j
+@RequestMapping("/account")
 public class AccountInfoController {
     @Autowired
     private AccountInfoService accountInfoService;
